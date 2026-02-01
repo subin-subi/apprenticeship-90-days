c68249601247514233087935841e136cf3bf1b67
subin-subi <subindass710@gmail.com>
2026-02-01 12:01:59 +0530

    Add Day-10 project

--- day-10/project/src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-function Loader() {
-  return (
-    <div className="text-center py-10 text-lg font-medium">
-      Loading jobs...
-    </div>
-  );
-}
-
-export default Loader;
--- day-10/project/src/services/jobApi.js
+++
@@ -1,7 +0,0 @@
-const API_URL = "https://remotive.com/api/remote-jobs";
-
-export async function getJobs() {
-  const response = await fetch(API_URL);
-  const data = await response.json();
-  return data.jobs.slice(0, 12); // limit jobs
-}
